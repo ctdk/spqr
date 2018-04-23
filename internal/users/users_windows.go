229e6658d76d3ed67dba5a962a47cdcd47cc5007
Jeremy Bingham <c-jeremy.bingham@apexlearning.com>
2018-04-23 08:08:08 -0700

    stub out windows support

--- internal/users/users_windows.go
+++ internal/users/users_windows.go
@@ -24,14 +24,92 @@ package users
 
 import (
 	"errors"
+	"github.com/ctdk/spqr/internal/processes"
+	"math/big"
+	"os"
 )
 
+// Obviously these need to change drastically
+const sshDirPerm = 0700
+const authKeyPerm = 0644
+const maxTmpDirNumBase int64 = 0xFFFFFFFF
+
+var maxTmpDirNum *big.Int
+
+const DefaultShell = "/bin/bash"
+const DefaultHomeBase = "/home"
+
+func init() {
+	maxTmpDirNum = big.NewInt(maxTmpDirNumBase)
+}
+
 var notImpErr = errors.New("Windows functionality is not implemented yet.")
+
+func osNew(userName string, fullName string, homeDir string, shell string, action UserAction, groups []string, authorizedKeys []string) (*User, error) {
+	return nil, notImpErr
+}
 
 func (u *User) osCreateUser() error {
 	return notImpErr
 }
 
-func New(userName string, fullName string, homeDir string, shell string, action UserAction, groups []string) (*User, error) {
+func (u *User) fillInUser() error {
+	return notImpErr
+}
+
+func (u *User) update() error {
+	return notImpErr
+}
+
+// rename?
+func (u *User) setNoLogin() error {
+	return notImpErr
+}
+
+func (u *User) changeShell(shell string) error {
+	return notImpErr
+}
+
+func (u *User) updateInfo(uEntry *UserInfo) error {
+	return notImpErr
+}
+
+func (u *User) getPrimaryGroup() (string, error) {
+	return "", notImpErr
+}
+
+func getDefaultShell() string {
+	return ""
+}
+
+func osMakeNewGroup(groupName string) error {
+	return notImpErr
+}
+
+func getShell(username string) (string, error) {
+	return "", notImpErr
+}
+
+func (u *User) clearExtraGroups() error {
+	return notImpErr
+}
+
+func (u *User) updateGroups() error {
+	return notImpErr
+}
+
+func (u *User) updateName() error {
+	return notImpErr
+}
+
+func (u *User) createTempAuthKeyFile(baseDir string) (*os.File, error) {
 	return nil, notImpErr
+}
+
+func (u *User) getUidGid() (int, int, error) {
+	return 0, 0, notImpErr
+}
+
+func (u *User) killProcesses() error {
+	return processes.KillUserProcesses(u.Uid)
 }
